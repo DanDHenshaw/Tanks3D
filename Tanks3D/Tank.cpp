e7679d6ddf56ca101e577b7bad8e4a0af4f8e2ae
Daniel Henshaw <danielhenshaw03@gmail.com>
2023-11-21 12:01:24 +0000

    Added working input to tank
    - Trigonometry to allow correct rotation/movement forward & backwards

--- Tanks3D/Tank.cpp
+++ Tanks3D/Tank.cpp
@@ -1,5 +1,10 @@
 #include "Tank.h"
 
+#include <cmath>
+
+using namespace DirectX;
+using namespace DirectX::SimpleMath;
+
 void Tank::Update(float dTime)
 {
 	GameObject3D::Update(dTime);
@@ -8,4 +13,40 @@ void Tank::Update(float dTime)
 void Tank::Initialise(D3D& d3d, std::string fileName)
 {
 
+}
+
+void Tank::Input(MouseAndKeys input, float dTime, unsigned short movUP, unsigned short movDOWN, unsigned short rotLEFT, unsigned short rotRight)
+{
+	bool keypressed = input.IsPressed(movUP) || input.IsPressed(movDOWN) ||
+		input.IsPressed(rotRight) || input.IsPressed(rotLEFT);
+
+	if (keypressed)
+	{
+		Vector3 pos(0, 0, 0);
+		Vector3 rot(0, 0, 0);
+
+		if (input.IsPressed(rotLEFT))
+		{
+			rot.y -= ROT_SPEED;
+		}
+		else if (input.IsPressed(rotRight))
+		{
+			rot.y += ROT_SPEED;
+		}
+
+		float angle = GetRotation().y + PI / 2;
+		if (input.IsPressed(movUP))
+		{
+			pos.x -= MOV_SPEED * cos(angle);
+			pos.z += MOV_SPEED * sin(angle);
+		}
+		else if (input.IsPressed(movDOWN))
+		{
+			pos.x += MOV_SPEED * cos(angle);
+			pos.z -= MOV_SPEED * sin(angle);
+		}
+
+		GetPosition() += pos * dTime;
+		GetRotation() += rot * dTime;
+	}
 }
--- Tanks3D/Tank.h
+++ Tanks3D/Tank.h
@@ -1,10 +1,20 @@
 #pragma once
 #include "GameObject.h"
 
+#include "Input.h"
+
 class Tank : public	GameObject3D
 {
 public:
+	using GameObject3D::GameObject3D;
+
 	void Update(float dTime) override;
+
+	void Input(MouseAndKeys input, float dTime, unsigned short movUP = VK_W, unsigned short movDOWN = VK_S, unsigned short rotLEFT = VK_A, unsigned short rotRight = VK_D);
+
+private:
+	float MOV_SPEED = 2.5f;
+	float ROT_SPEED = 5.0f;
 
 private:
 	void Initialise(D3D& d3d, std::string fileName) override;
